59e1b435672daf047ae751cc0f8b5d4ee803b87b
Mike Gulline <mike@mikegulline.com>
2023-06-12 22:26:05 -0700

    adde useEffect for window.navigator

--- src/components/emoji/emoji.tsx
+++ src/components/emoji/emoji.tsx
@@ -1,10 +1,17 @@
 'use client';
 
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 
 // show emojis on Mac only
 
 export default function Emoji({ children }: { children: ReactNode }) {
-  if (!window.navigator.userAgent.includes('Mac')) return null;
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    if (window.navigator.userAgent.includes('Mac')) setShow(true);
+  }, []);
+
+  if (!show) return null;
+
   return <>{children} </>;
 }
