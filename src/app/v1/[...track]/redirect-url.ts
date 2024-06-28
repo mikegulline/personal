0810358424f5323dcaed72eb064066f38b97bbd6
Mike Gulline <mike@mikegulline.com>
2024-06-28 16:09:02 -0700

    added resume to redirects

--- src/app/v1/[...track]/redirect-url.ts
+++ src/app/v1/[...track]/redirect-url.ts
@@ -4,6 +4,7 @@ type RedirectUrl = {
 
 export const redirectUrl: RedirectUrl = {
   portfolio: 'https://www.gulline.com/works',
+  resume: 'https://www.gulline.com/resume',
   lilink: 'https://www.gulline.com/works',
   libutton: 'https://www.gulline.com/works',
   githublink: 'https://www.gulline.com/works',
