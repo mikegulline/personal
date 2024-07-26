f3b89fb3b03358b804938b13b76820b492ee18fb
Mike Gulline <mike@mikegulline.com>
2024-07-26 16:57:37 -0700

    new

--- src/app/v1/[...track]/redirect-url.ts
+++ src/app/v1/[...track]/redirect-url.ts
@@ -18,4 +18,5 @@ export const redirectUrl: RedirectUrl = {
   'we-submit': `mailto:${process.env.EMAIL_ADDRESS}?subject=OMG…%20You're%20Hired!%20%F0%9F%A4%98%F0%9F%8F%BB&body=Hello Michael,%0D%0A%0D%0AYour resume looks amazing and your coverletter was a hoot!%0D%0A%0D%0AJudging by your skills and background, I… no, no… We think you would make an excellent addition to our team as Solutions Software Engineer L5.%0D%0A%0D%0ADo you have a free moment to speak about your future?%0D%0A%0D%0AFriends forever,%0D%0A`,
   linkedin: 'https://www.linkedin.com/in/mikegulline/',
   github: 'https://github.com/mikegulline',
+  none: 'https://github.com/mikegulline',
 };
